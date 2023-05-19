6437e1d5f785cb1c7dcd78b3cc72fee5b7a1fb82
ArturSzczypta <arturszczypta@gmail.com>
2023-05-19 22:54:26 +0200

    Preparing Index scraping

---
+++ Indexes/scrape_indexes.py
@@ -0,0 +1,24 @@
+''' Web scraping most importqnt indexes from the web'''
+
+'''
+Indexes:
+Germany: DAX Index (Deutscher Aktienindex) https://finance.yahoo.com/quote/DAX/history?period1=1420070400&period2=1684454400&interval=1d&filter=history&frequency=1d&includeAdjustedClose=true
+France: CAC 40 Index (Cotation Assistée en Continu) https://finance.yahoo.com/quote/%5EFCHI/history?period1=1420070400&period2=1684454400&interval=1d&filter=history&frequency=1d&includeAdjustedClose=true
+United Kingdom: FTSE 100 Index (Financial Times Stock Exchange 100 Index) -
+Europe: EURO STOXX 50 Index (representing the 50 largest companies in the Eurozone) https://finance.yahoo.com/quote/%5ESTOXX50E/history?period1=1420070400&period2=1691452800&interval=1d&filter=history&frequency=1d&includeAdjustedClose=true
+
+United States: S&P 500 Index (Standard & Poor's 500 Index) and Dow Jones Industrial Average (DJIA) https://finance.yahoo.com/quote/%5EGSPC/history?period1=1420070400&period2=1684454400&interval=1d&filter=history&frequency=1d&includeAdjustedClose=true
+China: SSE Composite Index (Shanghai Stock Exchange Composite Index) and Hang Seng Index (Hong Kong)
+India: BSE Sensex (Bombay Stock Exchange Sensex) and Nifty 50 (National Stock Exchange Fifty)
+Additionally, you can consider sector-specific indexes that represent specific industries:
+
+IT Sector: NASDAQ Composite Index (United States) and STOXX Global 200 Technology Index (global)
+Banking Sector: KBW Bank Index (United States) and EURO STOXX Banks Index (Europe)
+Manufacturing Sector: S&P Global Industrials Index (global) and EURO STOXX Industrial Goods & Services Index (Europe)
+If you are interested in tracking raw material prices, you can scrape the following:
+
+Crude Oil: Brent Crude Oil Price or West Texas Intermediate (WTI) Crude Oil Price
+Iron Ore: Iron Ore 62% Fe CFR China Spot Price
+Copper: Copper High Grade (COMEX) Futures Price
+Natural Gas: Henry Hub Natural Gas Spot Price
+'''
