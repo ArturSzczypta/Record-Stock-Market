70a1e51dae19707f406710e977648a0833689e53
ArturSzczypta <arturszczypta@gmail.com>
2024-01-14 09:55:37 +0100

    Refactoring gpw scraping code

--- scripts/sql_connector.py
+++ scripts/sql_connector.py
@@ -3,23 +3,28 @@
 import os
 from dotenv import load_dotenv
 import psycopg2
+import logging
 
 # Load environment variables from .env file
 load_dotenv('../.env.txt')
 
 
-def postgresql_connector(host, user, password, database):
-    conn = psycopg2.connect(host=host, user=user, password=password, database=database)
+def postgresql_connector(host: str, user: str, password: str,
+                         database: str) -> psycopg2.connect:
+    '''Connect to PostgreSQL database'''
+
+    conn = psycopg2.connect(host=host, user=user, password=password,
+                            database=database)
     return conn
 
 
-def check_connection(client):
+def check_connection(client: psycopg2.connect) -> None:
     ''' Check connection to database'''
     try:
         client.admin.command('ping')
-        print("Pinged your deployment. You successfully connected to MongoDB!")
+        logging.debug("You successfully connected to db")
     except Exception as e:
-        print(f'connection to db failed: {e}')
+        print(f'Connection to db failed: {e}')
 
 
 if __name__ == '__main__':
@@ -29,5 +34,7 @@ if __name__ == '__main__':
     password = os.getenv('POSTGRESQL_PASSWORD')
     database = os.getenv('POSTGRESQL_DATABASE')
 
-    conn = postgresql_connector(host='localhost', user='postgres', password='postgres', database='postgres')
+    conn = postgresql_connector(host='localhost', user='postgres',
+                                password='postgres', database='postgres')
     check_connection(conn)
+    conn.close()
--- scripts/stock_db_functions.py
+++ scripts/stock_db_functions.py
@@ -1,158 +1,75 @@
-'''
-Methods for web scraping stock records
-'''
-import os
+''' Functions for creating and writing to database'''
 import logging
-from logging import config
-import json
-import traceback
-import re
-import mariadb
 import pandas as pd
-import requests
+import datetime
 
 
+def create_table(conn, table_name):
+    '''Create table in database
+    :conn: connector to database
+    :table_name: name of table in database'''
 
-
-
-def add(given_df, given_date, new):
-    '''
-    Add new stocks
-    :given_df: pandas dataframe, layout as with polish stock exchange GPW
-    :given_date: date given by main script
-    :new: set of new stocks
-    '''
-
-    # Check if set is empty
-    if not new:
-        # return immediately if the set is empty
-        return
-
-    # Prepare dataframe with new listings
-    # Select first two columns
-    new_df = given_df[['name', 'code_isin']].copy()
-    # Select only new listings
-    new_df = new_df.loc[new_df['name'].isin(new)]
-
-    # Add listing date as given
-    new_df.loc[:, 'listed'] = given_date
-
-    # Add new stocks to SQL table stocks_recorded
-    add_stock = ("INSERT INTO stocks_recorded "
-                 "(name, code_isin, listed, delisted, relisted,  relist_count) "
-                 "VALUES (%s, %s, %s, NULL, NULL, NULL)")
-
-    # Instantiate MariaDB cursor
-    conn = database_connector()
+    # Check if table already exists
     mycursor = conn.cursor()
-    for row in new_df.itertuples(index=False):
-        # row[0] name, row[1] code_isin, row[2] date given
-        try:
-            mycursor.execute(add_stock, row)
-            conn.commit()
-        except:
-            log_exception('add', f' - Variables in {row=}')
-
-    # Get all current tables
     mycursor.execute("SHOW TABLES")
     current_tables = set()
     for row in mycursor:
         current_tables.add(row[0])
-    # Don't remove stocks_recorded
-    current_tables.remove('stocks_recorded')
-
-    # Create new tables
-    # Prepare dataframe table names
-    new_tables_df = pd.DataFrame({'name': list(new)})
-    new_tables_df['name'] = 't_' + new_tables_df['name'].astype(str).str.lower()
-
-    # Create SQL table for each new stock
-    TABLE_NAMES_WHITELIST = frozenset(new_tables_df['name'])
-
-    # Add new stocks to stocks_recorded
-    # Check for unexpected table names
-    for table_name in TABLE_NAMES_WHITELIST:
-        if table_name not in TABLE_NAMES_WHITELIST:
-            log_exception('add',
-                          f' - Possiple SQL injection attempt in {table_name=}')
-
-        # Check if table already exists
-        if table_name not in current_tables:
-            try:
-                mycursor.execute("CREATE TABLE {table_name} "
-                                 "(date_rec DATE NOT NULL PRIMARY KEY, "
-                                 "price_openning FLOAT, "
-                                 "price_max FLOAT, "
-                                 "price_min FLOAT, "
-                                 "price_closing FLOAT, "
-                                 "change_percent FLOAT, "
-                                 "traded_pieces INT, "
-                                 "number_transactions INT, "
-                                 "trading_value_in_1000 FLOAT);"
-                                 .format(table_name=table_name))
-                conn.commit()
-            except:
-                print(f'Cannot create table {table_name}')
-    # Create hierarchy of loggers
-    logger = logging.getLogger('write_results')
-    logger.debug(f'Adding stocks finished for {given_date}')
+    if table_name not in current_tables:
+        try:
+            mycursor.execute("CREATE TABLE {table_name} "
+                             "(date_rec DATE NOT NULL PRIMARY KEY, "
+                             "currency VARCHAR(3),"
+                             "price_openning FLOAT, "
+                             "price_max FLOAT, "
+                             "price_min FLOAT, "
+                             "price_closing FLOAT, "
+                             "change_percent FLOAT, "
+                             "traded_pieces INT, "
+                             "number_transactions INT, "
+                             "trading_value_in_1000 FLOAT);"
+                             .format(table_name=table_name))
+            conn.commit()
+        except Exception as e:
+            print(f'Cannot create table {table_name}, {e}')
     mycursor.close()
 
 
-def write_results(given_df, given_date):
-    '''
-    Write new stock resutls to seperate table for each stock
+def write_to_db(db_connector, table_name: str, data: pd.DataFrame,
+                given_date: datetime) -> None:
+    '''Add new results to database
+    :db_connector: connector to database
+    :table_name: name of table in database
+    :data: pandas dataframe with stock results
+    :given_date: date given by main script'''
 
-    :given_df: pandas dataframe, layout as with polish stock exchange GPW
-    :given_date: date given by main script
-    '''
-    # given_df   - pandas dataframe, layout as with polish stock exchange GPW
-    # given_date - date given by main script
+    # Return if no data
+    if not data:
+        return
 
-    # Prepare database
-    given_df['name'] = 't_' + given_df['name'].astype(str).str.lower()
-    given_df = given_df.drop(['code_isin', 'currency'], axis=1)
-    given_df.insert(1, 'given_date', given_date)
+    # Add date to dataframe
+    data.insert(1, 'date', given_date)
 
-    # Instantiate MariaDB cursor
-    conn = database_connector()
-    mycursor = conn.cursor()
-    TABLE_NAMES_WHITELIST = frozenset(given_df['name'])
-
-    for row in given_df.itertuples(index=False):
-        '''
-        row[0] table_name, row[1] date, row[2] price_openning,
-        row[3] price_max, row[4] price_min, row[5] price_closing,
-        row[6] change_percent, row[7] traded_pieces,
-        row[8] number_transactions, row[9] trading_value_in_1000
-
-        '''
-        # Check for unexpected table names
-        if row[0] not in TABLE_NAMES_WHITELIST:
-            log_exception('write_results',
-                          f' - Possiple SQL injection attempt in {row[0]}')
+    for row in data.itertuples(index=False):
 
         # Prepare SQL querry
         sql_string = '''INSERT INTO {0}
-        (date_rec, price_openning, price_max, price_min, price_closing,
-         change_percent, traded_pieces, number_transactions,
-        trading_value_in_1000) VALUES ('{1}', {2}, {3}, {4}, {5}, {6}, {7},
-         {8}, {9})'''.format(row[0],row[1],row[2], row[3], row[4], row[5],
-                             row[6], row[7], row[8], row[9])
+        (date, currency, price_openning, price_max, price_min,
+        price_closing, change_percent, traded_pieces, number_transactions,
+        trading_value_in_1000) VALUES ('{1}', {2}, {3}, {4}, {5}, {6},
+        {7}, {8}, {9}, {10}, {11})'''.format(table_name, row[0], row[1],
+                                             row[2], row[3], row[4], row[5],
+                                             row[6], row[7], row[8], row[9],
+                                             row[10])
 
         # Append all stock tables
         try:
-            mycursor.execute(sql_string)
-            conn.commit()
-        except:
-            log_exception('write_results',
-                          f' - Cannot write to stocks tables {sql_string}')
-    # Create hierarchy of loggers
-    logger = logging.getLogger('write_results')
-    logger.debug(f'Appending stocks results finished for {given_date}')
-    mycursor.close()
+            db_connector.execute(sql_string)
+            db_connector.commit()
+        except Exception as e:
+            logging.error(f'Cannot add results from {given_date}'
+                          f'to database, {e}')
+    logging.info(f'Added results from {given_date} to database')
 
-
-
-if __name__ == '__main__':
-    print('main')
+    # Terminate connection
+    db_connector.close()
